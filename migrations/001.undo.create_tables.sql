2972e842ff3ca8cfdaa615436d650a347c30889f
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-06-14 15:55:42 +0300

    migration 1

---
+++ migrations/001.undo.create_tables.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS project_managers_projects;
+DROP TABLE IF EXISTS clients_projects;
+DROP TABLE IF EXISTS contractors_projects;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS project_statuses;
+DROP TABLE IF EXISTS roles;
+
+
