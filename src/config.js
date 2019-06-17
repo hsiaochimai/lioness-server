83b4ac52b56fa5f8f18ff2d64c19ffc40554eea0
hsiaochimai <jchmai21@gmail.com>
2019-06-17 08:43:20 -0700

    refactored and wrote some tests!

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DATABASE_URL || 'postgresql://hsiaochimai@localhost:5432/lioness', 
-    TEST_DB_URL: process.env.TEST_DB_URL
+    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://hsiaochimai@localhost:5432/lioness-test'
   }
---
+++ src/projects/projects-router.js
@@ -0,0 +1,37 @@
+// const path = require("path");
+const express = require("express");
+// const xss = require("xss");
+const ProjectsService = require("./projects-service");
+const projectsRouter = express.Router();
+// const jsonParser = express.json();
+const SORT_ASC = 'ASC'
+const SORT_DESC = 'DESC'
+const ITEMS_PER_PAGE = 10
+
+const STATUS_ESTIMATE = 1
+const STATUS_IN_PROGRESS = 2
+const STATUS_BILLED = 3
+const STATUS_OTHER = 4
+
+const projectsDefaultOptions = {
+  statusFilter: null,
+  searchQuery: null,
+  budgetSort: SORT_ASC,
+  dateTypeFilter: null,
+  dateSort: null,
+  afterDate: null,
+  beforeDate: null,
+  roleFilter: null,
+  pageNumber: 1,
+}
+
+projectsRouter
+.route('/')
+.get( async (req, res) => {
+    const knex = req.app.get("db");
+    const mergedOpts = { ...projectsDefaultOptions, ...req.query }
+    const result = await ProjectsService.getProjects(knex, mergedOpts)
+    res.json(result);
+  });
+
+  module.exports = projectsRouter;
