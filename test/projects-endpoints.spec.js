0bd6fcce6eecab991c95ccfff192dff66ea4bc43
hsiaochimai <jchmai21@gmail.com>
2019-06-21 08:21:24 -0700

    projects sort test works

--- test/projects-endpoints.spec.js
+++ test/projects-endpoints.spec.js
@@ -1,28 +1,23 @@
 const {
   FETCH_INFO: { SORT_ASC, SORT_DESC, ITEMS_PER_PAGE },
-  ROLES: { ADMIN_ROLE, CLIENT_ROLE, MANAGER_ROLE, CONTRACTOR_ROLE }, 
+  ROLES: { ADMIN_ROLE, CLIENT_ROLE, MANAGER_ROLE, CONTRACTOR_ROLE },
   projectsDefaultOptions
- } = require('../src/config')
- import queryString from 'query-string'
+} = require("../src/config");
+const queryString = require("query-string");
 // const knex = require("knex");
 const app = require("../src/app");
-const {
-  makeTestKnex,
-  populateDB,
-  fixturesData,
-} = require('./helpers')
-const { clearData } = require('../seeds/seedData')
+const { makeTestKnex, populateDB, fixturesData } = require("./helpers");
+const { clearData } = require("../seeds/seedData");
 
-const { TEST_DB_URL } = require('../src/config')
+const { TEST_DB_URL } = require("../src/config");
 const { API_TOKEN } = process.env;
-const ProjectService =require('../src/projects/projects-service')
+const ProjectService = require("../src/projects/projects-service");
 const { expect } = require("chai");
-describe("Projects Endpoints", function () {
-  let db;
-
+let db;
+describe("Projects Endpoints", function() {
   before(() => {
-    db = makeTestKnex()
-    app.set("db", db)
+    db = makeTestKnex();
+    app.set("db", db);
   });
 
   // before(async () => {
@@ -30,8 +25,8 @@ describe("Projects Endpoints", function () {
   // })
 
   after(async () => {
-    await db.destroy()
-    console.log('server closed')
+    await db.destroy();
+    console.log("server closed");
   });
 
   // afterEach("cleanup", () => db.raw('TRUNCATE projects, project_statuses, users RESTART IDENTITY CASCADE'));
@@ -39,13 +34,17 @@ describe("Projects Endpoints", function () {
   describe(`GET /api/projects`, async () => {
     context(`Given no projects`, () => {
       it(`responds with 200 and an empty list`, async () => {
-        await clearData(db)
+        await clearData(db);
         // await populateDB(db)
         return supertest(app)
           .get("/api/projects")
           .set("Authorization", `Bearer ${API_TOKEN}`)
-          .expect(200, { data: [], numPages: 0, totalItemCount: 0,   params:
-            projectsDefaultOptions  });
+          .expect(200, {
+            data: [],
+            numPages: 0,
+            totalItemCount: 0,
+            params: projectsDefaultOptions
+          });
       });
     });
     context("Given there are projects in the database", async () => {
@@ -54,18 +53,55 @@ describe("Projects Endpoints", function () {
       // const testRoles = fixturesData.roles;
       // const testProjects = fixturesData.projects
       // const testContractorProjects = fixturesData.contractor_projects
-      let opts={...projectsDefaultOptions, budgetSort= SORT_ASC}
-      const qs = queryString.stringify(opts)
-      it("GET /api/projects responds with 200 and sorts by budget", async () => {
-        await populateDB(db)
-        return supertest(app)
-          .get(`/api/projects/?${qs}`)
-          .expect(200,ProjectService.getProjects(db, budgetSort=ASC));
-      })
+
+      it("GET /api/projects responds with 200 and sorts by budget ASC", async () => {
+        await populateDB(db);
+
+        let opts = { ...projectsDefaultOptions, budgetSort: SORT_ASC };
+        const qs = queryString.stringify(opts);
+        const serviceResult = await ProjectService.getProjects(db, opts);
+
+        return (
+          supertest(app)
+            .get(`/api/projects/?${qs}`)
+            // .expect(200,ProjectService.getProjects(db, budgetSort=ASC));
+            .expect(200)
+            .then(response => {
+              const { data } = response.body;
+              //sort function
+              data.forEach((i, index) => {
+                if (index < data.length - 2) {
+                  expect(data[index].budget < data[index + 1].budget).to.be.true;
+                }
+              });
+              //filter .forEach
+            })
+        );
+      });
+      it("GET /api/projects responds with 200 and sorts by budget ASC", async () => {
+        await populateDB(db);
+
+        let opts = { ...projectsDefaultOptions, budgetSort: SORT_ASC };
+        const qs = queryString.stringify(opts);
+        const serviceResult = await ProjectService.getProjects(db, opts);
+
+        return (
+          supertest(app)
+            .get(`/api/projects/?${qs}`)
+            // .expect(200,ProjectService.getProjects(db, budgetSort=ASC));
+            .expect(200)
+            .then(response => {
+              const { data } = response.body;
+              //sort function
+              data.forEach((i, index) => {
+                if (index < data.length - 2) {
+                  expect(data[index].budget < data[index + 1].budget).to.be.true;
+                }
+              });
+              //filter .forEach
+            })
+        );
+      });
     });
-  })
-
-
-
-
-})
+  });
+});
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,6 @@
-const { expect } = require('chai')
+const { expect,assert } = require('chai')
 const supertest = require('supertest')
 
+global.assert = assert
 global.expect = expect
 global.supertest = supertest
