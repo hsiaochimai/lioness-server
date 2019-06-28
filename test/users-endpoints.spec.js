2b90a1b8583dc9a8d385b9bafdec32b260886658
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-06-28 18:43:00 +0300

    WIP; JWT auth

--- test/users-endpoints.spec.js
+++ test/users-endpoints.spec.js
@@ -3,9 +3,14 @@ const {
   ROLES: { ADMIN_ROLE, CLIENT_ROLE, MANAGER_ROLE, CONTRACTOR_ROLE },
   usersDefaultOptions
 } = require("../src/config");
+
+// require('es6-promise').polyfill();
+// require('isomorphic-fetch');
+
 const queryString = require("query-string");
 const passport = require('passport');
 const request = require('supertest')
+const { setKnexInstance } = require('../src/auth/strategies')
 const jwtAuth = passport.authenticate('jwt', { session: false });
 // const knex = require("knex");
 const app = require("../src/app");
@@ -16,33 +21,60 @@ const { TEST_DB_URL } = require("../src/config");
 const { API_TOKEN } = process.env;
 const UsersService = require("../src/users/users-service");
 const { expect, assert } = require("chai");
-let authToken;
-let db;
 
-describe("Projects Endpoints", function() {
-  const testLogin ={
-    email:"Mervin.Graham@hotmail.com",
-    password:"GAfJ8cFYg2J1SdS"
-  }
-  before( () => {
+let db;
+let authToken
+
+const doLogin = () => supertest(app)
+  .post(
+    '/api/auth/login')
+
+  // method: 'POST',
+  .send({
+    email: 'Mervin.Graham@hotmail.com',
+    password: 'GAfJ8cFYg2J1SdS',
+  })
+  .set('Accept', 'application/json')
+  .expect('Content-Type', /json/)
+
+
+
+  .then(r => {
+    console.log('AUTH RESP', r.body)
+    // process.exit(0)
+    authToken = r.body.authToken
+  })
+
+
+
+describe("Projects Endpoints", function () {
+  // const testLogin = {
+  //   email: "Mervin.Graham@hotmail.com",
+  //   password: "GAfJ8cFYg2J1SdS"
+  // }
+  before(async () => {
     db = makeTestKnex();
     app.set("db", db);
-    console.log(`this is db`,db)
-//     await populateDB(db)
-//    await request(app)
-//     .post('/api/auth/login')
-//     .send(testLogin)
-// .then( response=>{
-//   console.log(`is this working`,response)
-//   done()
-})
-// .end((response)=>{
-//   console.log(`hello auth`,response.body.token)
-//   authToken = response.body.token;
-//   done();
-// })
-  
- 
+    setKnexInstance(db)
+
+    await doLogin()
+
+
+    //     await populateDB(db)
+    //    await request(app)
+    //     .post('/api/auth/login')
+    //     .send(testLogin)
+    // .then( response=>{
+    //   console.log(`is this working`,response)
+    //   done()
+  })
+  // .end((response)=>{
+  //   console.log(`hello auth`,response.body.token)
+  //   authToken = response.body.token;
+  //   done();
+  // })
+
+
   // before(async () => {
 
   // })
@@ -59,6 +91,7 @@ describe("Projects Endpoints", function() {
         // await populateDB(db)
         return supertest(app)
           .get("/api/users")
+          .set({ Authorization: `Bearer ${authToken}` })
           .expect(200, {
             data: [],
             numPages: 0,
@@ -74,9 +107,11 @@ describe("Projects Endpoints", function() {
         let opts = { ...usersDefaultOptions, userNameSort: SORT_ASC };
         const qs = queryString.stringify(opts);
         await UsersService.getUsers(db, opts);
+        await doLogin()
 
         return supertest(app)
           .get(`/api/users/?${qs}`)
+          .set({ Authorization: `Bearer ${authToken}` })
           .expect(200)
           .then(response => {
             const { data } = response.body;
@@ -140,8 +175,8 @@ describe("Projects Endpoints", function() {
         user.email = "destroysociety33@hotmail.com";
         user.phone = "703-724-9988";
         user.full_name = "Christine Oberlin";
-        user.role_id=2,
-        user.password="Ihopethisworks1"
+        user.role_id = 2,
+          user.password = "Ihopethisworks1"
         const body = {
           user
         };
@@ -163,22 +198,22 @@ describe("Projects Endpoints", function() {
       });
       it("Deletes a user", async () => {
         await populateDB(db);
-        const idToRemove= 3;
-      
+        const idToRemove = 3;
+
         return supertest(app)
-            .delete(`/api/users/id/${idToRemove}`)
-            // .expect(200,ProjectService.getProjects(db, budgetSort=ASC));
-            .expect(204)
-            .then(
-              supertest(app)
+          .delete(`/api/users/id/${idToRemove}`)
+          // .expect(200,ProjectService.getProjects(db, budgetSort=ASC));
+          .expect(204)
+          .then(
+            supertest(app)
               .get(`/api/users/id/${idToRemove}`)
-              .then(r=>JSON.parse(r.text))
-              .then(res=>{
-                expect(res.inactive===true).to.be.true
+              .then(r => JSON.parse(r.text))
+              .then(res => {
+                expect(res.inactive === true).to.be.true
               })
-              )
-             
+          )
+
+      });
     });
   });
-});
 })
